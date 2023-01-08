886d37ea4a4415de512e96e1a57ef4ae38caaaa0
Luciana Bossi Cardenas <lucianafbossi@gmail.com>
2023-01-08 09:29:33 +0100

    storing user category

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //getting html elements
 const selectMenu = document.getElementById('categories');
+const buttonOk = document.getElementById('btnOk');
 
 //getting categories from API
 async function getCategories() {
@@ -19,9 +20,31 @@ async function showCategories() {
     const optionsName = Object.getOwnPropertyNames(options.contents.categories);
     for(let i=0; i< optionsName.length; i++) {
         const newOption = document.createElement('option');
+        newOption.value = optionsName[i];
         newOption.innerText = optionsName[i];
         selectMenu.appendChild(newOption);
     }
 };
 
 showCategories();
+
+//storing user category
+var userCategory;
+selectMenu.addEventListener('change', (event) => {
+    userCategory = selectMenu.value;
+});
+//getting quotes from api
+async function getQuote() {
+    try {
+        const category = `http://quotes.rest/qod.json?category=${userCategory}`;
+        const res = await fetch(category);
+        const data = res.json();
+        return data;
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+buttonOk.addEventListener('click', (event) => {
+    console.log(getQuote());
+})
